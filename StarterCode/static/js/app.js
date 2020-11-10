a65d192a93b776345fc260720fdf98e26f3c2ca7
BillRigg <riggiobill@gmail.com>
2020-11-10 13:36:59 -0500

    Function Skeleton
    Set up a function skeleton to establish the flow of the code and what work I need to do going forward.

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,36 @@
+//build a map of functions, test and fill in as you go
+
+function init() {
+    // D3 select the dropdown element
+
+    // Fill in the select options using the samples.json file
+
+
+    // call buildCharts
+    // call buildMetadata
+}
+
+function buildMetadata(data) {
+
+    // filter the data for results, store in array, access object items
+
+}
+
+function buildCharts(data) {
+    
+    // D3 json to select the sample data
+
+    // build bar chart
+
+    // plotly call
+
+
+
+    // build bubble chart
+
+    // plotly call
+
+}
+
+// Initialize dashboard with info
+init();
